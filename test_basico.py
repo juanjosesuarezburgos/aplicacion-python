bb21af47f31a1fbb93b6c1e3870f676fc3be20fa
juanjosesuarezburgos <juanjose.suarezburgos.st@nttdata.com>
2026-02-13 12:20:58 +0100

    Add basic unit test for always true condition

---
+++ test_basico.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestEjemplo(unittest.TestCase):
+    
+    def test_algo_que_siempre_es_cierto(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
